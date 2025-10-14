f3ad803944deaac06ee2abd3888862189dc1f2e2
Weidsom Nascimento <source@snakesecurity.org>
2025-10-14 12:00:14 -0700

    Force double fork usage

--- src/selfupdate.c
+++ src/selfupdate.c
@@ -8,19 +8,39 @@
 
 void start_update_via_script() {
 
-    pid_t pid = fork();
+    pid_t pid1 = fork();
 
-    if (pid == -1) {
-        perror("AFOS-NG Self UPDATE Failed: Fork CREATION");
+    if (pid1 == -1) {
+        perror("AFOS-NG Self UPDATE Failed: First Fork CREATION");
         return; 
     }
 
-    if (pid == 0) {        
-        printf("AFOS-NG Fork STARTED: Running: %s\n", UPDATE_SCRIPT);
-        execlp("bash", "bash", UPDATE_SCRIPT, (char *)NULL);
-        system("rm -rf /opt/AFOS/afos");
-        exit(0);
-    } else {
+    if (pid1 > 0) {
+        
         exit(0); 
     }
+
+
+    if (setsid() == -1) {
+        perror("AFOS-NG Self UPDATE Failed: setsid");
+        exit(1);
+    }
+    
+    pid_t pid2 = fork();
+    
+    if (pid2 == -1) {
+        perror("AFOS-NG Self UPDATE Failed: Second Fork CREATION");
+        exit(1);
+    }
+    
+    if (pid2 > 0) {        
+        exit(0); 
+    }
+    
+    printf("AFOS-NG GC STARTED: Running: %s\n", UPDATE_SCRIPT);
+
+    execlp("bash", "bash", UPDATE_SCRIPT, (char *)NULL);
+    
+    perror("AFOS-NG GC FAILED: execlp");
+    exit(1); 
 }

3d4c6e8a2d615094a24fac4e42e316628f02b62e
Weidsom Nascimento <weidsom@snakesecurity.org>
2026-05-31 21:36:58 -0300

    Add instruction to load helper function files

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -10,3 +10,36 @@ else
   # houston we have a problem
   exit 1
 fi
+
+cp -Rf ../UTILS/afos_shell_functions.sh /opt/AFOS/
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy afos_shell_functions... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+chown -R root:root /opt/AFOS/afos_shell_functions.sh
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Set afos_shell_functions owner... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+chmod 755 /opt/AFOS/afos_shell_functions.sh
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Set afos_shell_functions permissions... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
